fa23345fc3b480d5ea632a6f773554bcb0d4f82e
Pallavi-Mukalla <pallavimukalla@gmail.com>
2026-02-16 14:45:31 +0530

    made few changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://healthsphereai.onrender.com',
+  origins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://healthsphereai.onrender.com"
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
